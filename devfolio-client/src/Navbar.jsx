6c4908721ab25bbe0a0962969183520fc2a1dfd2
YoussefNasser121 <youssefnasser1213@gmail.com>
2026-06-24 12:46:50 +0300

    Update Navbar.jsx

--- devfolio-client/src/Navbar.jsx
+++ devfolio-client/src/Navbar.jsx
@@ -1,30 +1,40 @@
-import { NavLink } from 'react-router-dom';
+// src/components/Navbar.jsx
+import { NavLink } from "react-router-dom";
 
-const Navbar = () => (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div className="container">
-      <span className="navbar-brand">Youssef Nasser's Portofolio</span>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ms-auto">
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/">
-              Home
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/projects">
-              Projects
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/contact">
-              Contact
-            </NavLink>
-          </li>
-        </ul>
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg" style={{ backgroundColor: "#051c4f" }}>
+      <div className="container-fluid">
+        <span className="navbar-brand text-light fw-bold">Youssef Nasser's Portfolio</span>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          style={{ borderColor: "rgba(255,255,255,0.5)" }}
+        >
+          <span className="navbar-toggler-icon" style={{ filter: "invert(1)" }}></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link text-light" to="/">Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link text-light" to="/projects">Projects</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link text-light" to="/contact">Contact</NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+}
 
 export default Navbar;
